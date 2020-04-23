87e3720c8bc62b6924a6d0937ba3e450d7578234
Amber-Pittman <amber@amberpittman.com>
2020-04-23 19:46:45 -0400

    Created CRUD endpoints and tested with Insomnia

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const accountsRouter = require("../data/seeds/accountsRouter");
 
-const db = require("../data/dbConfig.js");
+const db = require("../data/dbConfig");
 
 const server = express();
 
